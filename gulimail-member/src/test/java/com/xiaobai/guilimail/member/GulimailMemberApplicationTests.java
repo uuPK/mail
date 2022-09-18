01a005fd7a28d8ad07ed4d0f1be3976e72a33f52
1442828431@qq.com <Ll1442828431>
2022-09-19 01:36:24 +0800

    init project

---
+++ gulimail-member/src/test/java/com/xiaobai/guilimail/member/GulimailMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaobai.guilimail.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimail-product/src/test/java/com/xiaobai/gulimail/product/GulimailProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaobai.gulimail.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
